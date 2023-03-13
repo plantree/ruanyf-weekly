cbce0d97a804f4ebcbc039dbeb52d06e411b33a9
plantree <wpy1174555847@outlook.com>
2023-03-13 10:20:58 +0800

    Update main.sh

--- scripts/main.sh
+++ scripts/main.sh
@@ -6,4 +6,4 @@ curl -s -L -O https://github.com/ruanyf/weekly/archive/refs/heads/master.zip
 unzip -o master.zip
 rm master.zip*
 node scripts/weekly.js > scripts/weekly.json
-cp ./weekly-master/docs/issue*.md ./docs/weekly/
+cp -n ./weekly-master/docs/issue*.md ./docs/weekly/
