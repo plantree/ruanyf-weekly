b7540a587cde3d3d5b394b277d125f6e67ec1a12
plantree <wpy1174555847@outlook.com>
2023-05-16 09:37:20 +0800

    [2023-05-16] add thumbnail to wechat share

---
+++ scripts/build-hook.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+function add_thumbnail() {
+    files=`ls . | grep .html`
+    for file in ${files[@]}
+    do 
+        sed -i '/<body>/a\<div style="display:none;"><img src="/thumbnail.jpg" alt=""></div>' ${file}
+    done
+}
+
+cd ./docs/.vitepress/dist
+add_thumbnail
+
+cd weekly
+add_thumbnail
