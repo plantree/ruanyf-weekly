49e681e23fc849f00af619a8e2fd7697a0053558
plantree <wpy1174555847@outlook.com>
2023-05-16 09:45:01 +0800

    [2023-05-16] fix a bug about thumbnail

--- scripts/build-hook.sh
+++ scripts/build-hook.sh
@@ -3,7 +3,7 @@ function add_thumbnail() {
     files=`ls . | grep .html`
     for file in ${files[@]}
     do 
-        sed -i '/<body>/a\<div style="display:none;"><img src="/thumbnail.jpg" alt=""></div>' ${file}
+        sed -i '/<body>/a\<div style="display:none;"><img src="https://ruanyf-weekly.plantree.me/thumbnail.jpg" alt=""></div>' ${file}
     done
 }
 
