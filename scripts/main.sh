13814483d28d14289423892adc4752c0fbcd55b5
plantree <wpy1174555847@outlook.com>
2023-08-21 11:39:42 +0800

    [2023-08-21] support `https` replace

--- scripts/main.sh
+++ scripts/main.sh
@@ -13,5 +13,5 @@ files=`ls . | grep issue`
 for file in ${files[@]}
 do 
     sed -i '/# 科技爱好者周刊/a\<img class="pv" src="https://api.visitor.plantree.me/visitor-badge/pv?namespace=plantree.me&key=ruanyf-weekly/'${file}'">\n' ${file}
-    sed -i 's/http:\/\/www.ruanyifeng.com\/blog\/.*issue-\([0-9]\+\)\.html/.\/issue-\1/' ${file}
+    sed -i 's/\(http\|https\):\/\/www.ruanyifeng.com\/blog\/.*issue-\([0-9]\+\)\.html/.\/issue-\2/' ${file}
 done
