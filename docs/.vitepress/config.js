5de6b6e6b4d1483dadddf4e70900b6e1f074be43
plantree <wpy1174555847@outlook.com>
2023-08-31 10:37:04 +0800

    [2023-08-31] add google analytics

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,14 +4,14 @@ import fs from 'fs'
 import path from 'path'
 
 export default defineConfig({
-  ignoreDeadLinks: true, 
+  ignoreDeadLinks: true,
   title: 'ruanyf-weekly',
   description: '阮一峰的技术周刊',
   lastUpdated: true,
   cleanUrls: true,
   lang: 'zh-CN',
   head: [
-    ['link', { rel: "icon", type: "image/png", href: "/favicon.png"}],
+    ['link', { rel: "icon", type: "image/png", href: "/favicon.png" }],
     ['script', { src: '/_vercel/insights/script.js', defer: '' }],
     ['meta', { name: 'theme-color', content: '#3c8772' }],
     ['meta', { property: 'og:type', content: 'website' }],
@@ -19,6 +19,13 @@ export default defineConfig({
     ['meta', { property: 'og:description', content: '科技, 分享, 开源' }],
     ['meta', { property: 'og:image', content: 'https://ruanyf-weekly.plantree.me/thumbnail.jpg' }],
     ['meta', { property: 'og:url', content: 'https://ruanyf-weekly.plantree.me/' }],
+    // google analytics
+    ['script', { async: true, src: 'https://www.googletagmanager.com/gtag/js?id=G-2QFP04Q1TY' }],
+    ['script', {}, `window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-2QFP04Q1TY');`],
   ],
   markdown: {
     headers: {
@@ -64,35 +71,35 @@ export default defineConfig({
 });
 
 function _convertParseWeeklyReadme() {
-    const content = fs.readFileSync('./scripts/weekly.json', 'utf8').toString()
-    const tree = JSON.parse(content)
-    const config = [];
-    for (let year in tree) {
-        const monthItems = [];
-        for (let month in tree[year]) {
-            const items = [];
-            for (let issue of tree[year][month]) {
-                items.push({
-                    text: issue[1].split('-')[1] + '期 | ' + issue[0],
-                    link: `/weekly/${issue[1]}`
-                });
-            }
-            monthItems.push({
-                text: month,
-                collapsed: true,
-                items: items
-            });
-        }
-        config.push({
-            text: year,
-            collapsed: true,
-            items: monthItems
+  const content = fs.readFileSync('./scripts/weekly.json', 'utf8').toString()
+  const tree = JSON.parse(content)
+  const config = [];
+  for (let year in tree) {
+    const monthItems = [];
+    for (let month in tree[year]) {
+      const items = [];
+      for (let issue of tree[year][month]) {
+        items.push({
+          text: issue[1].split('-')[1] + '期 | ' + issue[0],
+          link: `/weekly/${issue[1]}`
         });
+      }
+      monthItems.push({
+        text: month,
+        collapsed: true,
+        items: items
+      });
     }
-    config.reverse();
-    config[0].collapsed = false;
-    config[0].items[0].collapsed = false;
-    return config
+    config.push({
+      text: year,
+      collapsed: true,
+      items: monthItems
+    });
+  }
+  config.reverse();
+  config[0].collapsed = false;
+  config[0].items[0].collapsed = false;
+  return config
 }
 
 function sidebarWeekly() {
